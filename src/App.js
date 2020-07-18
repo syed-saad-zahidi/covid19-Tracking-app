639a23b8bc19795a50f1f24a323db261a6cd1bb9
syed-saad-zahidi <59394889+syed-saad-zahidi@users.noreply.github.com>
2020-07-18 21:23:31 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import NavBar from './Components/Navbar';
 import InfoPanel from './Components/InfoPanel';
 import FootNav from './Components/footNav'
 
 function App() {
-  
+  const screenConfig = useState(0);
   return (
     <div className="App">
      <NavBar />
-     <InfoPanel/>
-     <FootNav / >
+     <InfoPanel currentScreen={screenConfig[0]}/>
+     <FootNav screenConfig={screenConfig} / >
     </div>
   );
 }
--- src/Components/footNav.js
+++ src/Components/footNav.js
@@ -15,15 +15,16 @@ const useStyles = makeStyles({
   },
 });
 
-export default function FootNav() {
+export default function FootNav({screenConfig}) {
   const classes = useStyles();
+  console.log(screenConfig);
   const [value, setValue] = React.useState(0);
 
   return (
     <BottomNavigation
-      value={value}
+      value={screenConfig[0]}
       onChange={(event, newValue) => {
-        setValue(newValue);
+        screenConfig[1](newValue);
       }}
       showLabels
       className={classes.root}
